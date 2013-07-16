87c432a0f8c453f4da5dceaa29b91fd3ff047510
Cyberpew <root@cyberpew.me>
2013-07-16 08:06:07 -0700

    Fix indentation & test IRC hook.

--- src/me/xir/radio/radiobot/Config.java
+++ src/me/xir/radio/radiobot/Config.java
@@ -7,28 +7,28 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
-	
-	 public static Properties config = new Properties();
-	 static String nick;
-	 static String user;
-	 static String server;
-	 static int port;
-	 static String pass;
-	 static String serverpass;
-	 static boolean SSL;
-	 static String[] channels;
-	 public static String[] admins;
-	 
-	 public static void loadConfig() throws FileNotFoundException, IOException {
-		 config.load(new FileInputStream("CyBot.properties"));
-		 nick = config.getProperty("nick");
-		 user = config.getProperty("user");
-		 server = config.getProperty("server");
-		 port = Integer.parseInt(config.getProperty("port"));
-		 SSL = Boolean.parseBoolean(config.getProperty("SSL"));
-		 pass = config.getProperty("pass");
-		 serverpass = config.getProperty("serverpass");
-		 channels = config.getProperty("channels").split(",");
-		 admins = config.getProperty("admins").split(",");
-	 }
+
+	public static Properties config = new Properties();
+	static String nick;
+	static String user;
+	static String server;
+	static int port;
+	static String pass;
+	static String serverpass;
+	static boolean SSL;
+	static String[] channels;
+	public static String[] admins;
+
+	public static void loadConfig() throws FileNotFoundException, IOException {
+		config.load(new FileInputStream("CyBot.properties"));
+		nick = config.getProperty("nick");
+		user = config.getProperty("user");
+		server = config.getProperty("server");
+		port = Integer.parseInt(config.getProperty("port"));
+		SSL = Boolean.parseBoolean(config.getProperty("SSL"));
+		pass = config.getProperty("pass");
+		serverpass = config.getProperty("serverpass");
+		channels = config.getProperty("channels").split(",");
+		admins = config.getProperty("admins").split(",");
+	}
 }
