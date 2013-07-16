1b90114cb918e3017aa7151076e430cb5067574e
Cyberpew <root@cyberpew.me>
2013-07-16 07:50:19 -0700

    Decided that I'll use a configuration file for the bot instead of hard coded.

---
+++ src/me/xir/radio/radiobot/Config.java
@@ -0,0 +1,34 @@
+package me.xir.radio.radiobot;
+
+//Java imports
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+	
+	 public static Properties config = new Properties();
+	 static String nick;
+	 static String user;
+	 static String server;
+	 static int port;
+	 static String pass;
+	 static String serverpass;
+	 static boolean SSL;
+	 static String[] channels;
+	 public static String[] admins;
+	 
+	 public static void loadConfig() throws FileNotFoundException, IOException {
+		 config.load(new FileInputStream("CyBot.properties"));
+		 nick = config.getProperty("nick");
+		 user = config.getProperty("user");
+		 server = config.getProperty("server");
+		 port = Integer.parseInt(config.getProperty("port"));
+		 SSL = Boolean.parseBoolean(config.getProperty("SSL"));
+		 pass = config.getProperty("pass");
+		 serverpass = config.getProperty("serverpass");
+		 channels = config.getProperty("channels").split(",");
+		 admins = config.getProperty("admins").split(",");
+	 }
+}
--- src/me/xir/radio/radiobot/RadioBot.java
+++ src/me/xir/radio/radiobot/RadioBot.java
@@ -3,11 +3,62 @@ package me.xir.radio.radiobot;
 //Java imports
 
 //PircBotX imports
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import org.pircbotx.PircBotX;
+import org.pircbotx.UtilSSLSocketFactory;
+import org.pircbotx.hooks.Listener;
+import org.pircbotx.hooks.ListenerAdapter;
 
 //RadioBot imports
+import me.xir.radio.radiobot.Config;
 
+public class RadioBot extends ListenerAdapter implements Listener {
 
-public class RadioBot {
+	public static PircBotX bot = new PircBotX();
+	public final static Logger logger = Logger.getLogger("RadioBot");
 
+	public static void main(String[] args) throws Exception, FileNotFoundException, IOException {
+		//Load Config
+		try {
+			Config.loadConfig();
+		} catch (FileNotFoundException ex) {
+			// Generate config if does not exist.
+		}
+
+		//Setup the bot with all the things
+		bot.setAutoNickChange(true);
+		bot.setVerbose(true);
+
+		bot.setVersion("RadioBot v0.1 - http://radio.xir.me");
+
+		bot.setLogin(Config.user);
+		bot.setName(Config.nick);
+		bot.identify(Config.pass);
+
+		//Connection start
+		if(Config.SSL && !Config.serverpass.isEmpty()) {
+			bot.connect(Config.server, Config.port, Config.serverpass, new UtilSSLSocketFactory().trustAllCertificates());			
+		} else if (Config.SSL && Config.serverpass.isEmpty()) {
+			bot.connect(Config.server, Config.port, new UtilSSLSocketFactory().trustAllCertificates());
+		} else {
+			bot.connect(Config.server, Config.port);
+		}
+
+		bot.setAutoReconnect(true);
+		bot.setAutoReconnectChannels(true);
+
+		joinChannels();
+		loadListeners();
+	}
+	public static void joinChannels() {
+		for (int i = 0; i < Config.channels.length; i++) {
+			bot.joinChannel(Config.channels[i]);
+		}
+	}
+	public static void loadListeners() throws Exception {
+		
+	}
 }
