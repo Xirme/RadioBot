b2bb2a6696669b88da40d8bd886042fbb0e5a6de
Cyberpew <root@cyberpew.me>
2013-08-08 00:14:19 -0700

    FUCKING WORK

--- src/me/xir/radio/radiobot/utils/Radio_Query.java
+++ src/me/xir/radio/radiobot/utils/Radio_Query.java
@@ -1,6 +1,7 @@
 package me.xir.radio.radiobot.utils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -40,14 +41,60 @@ public class Radio_Query extends ListenerAdapter {
 			  RadioBot.bot.sendMessage("#radio","Current Song: " + currentsong); 
 		    }
 	}
-*/
-		  URL url = new URL("http://dev.cyberpew.me/stats.xml");
-		  HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		  conn.setRequestMethod("GET");
-		  conn.setAllowUserInteraction(false);
-		  conn.setDoInput(true);
-		  conn.setDoOutput(true);
-		  conn.connect();
+*/  
+		int BUFFER_SIZE = 4096;
+		String fileURL = "http://dev.cyberpew.me/stats.xml";
+		  URL url = new URL(fileURL);
+	        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+	        int responseCode = httpConn.getResponseCode();
+	 
+	        // always check HTTP response code first
+	        if (responseCode == HttpURLConnection.HTTP_OK) {
+	            String fileName = "";
+	            String disposition = httpConn.getHeaderField("Content-Disposition");
+	            String contentType = httpConn.getContentType();
+	            int contentLength = httpConn.getContentLength();
+	 
+	            if (disposition != null) {
+	                // extracts file name from header field
+	                int index = disposition.indexOf("filename=");
+	                if (index > 0) {
+	                    fileName = disposition.substring(index + 10,
+	                            disposition.length() - 1);
+	                }
+	            } else {
+	                // extracts file name from URL
+	                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
+	                        fileURL.length());
+	            }
+	 
+	            System.out.println("Content-Type = " + contentType);
+	            System.out.println("Content-Disposition = " + disposition);
+	            System.out.println("Content-Length = " + contentLength);
+	            System.out.println("fileName = " + fileName);
+	 
+	            // opens input stream from the HTTP connection
+	            InputStream inputStream = httpConn.getInputStream();
+	            String saveFilePath = "" + File.separator + fileName;
+	             
+	            // opens an output stream to save into file
+	            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+	 
+	            int bytesRead = -1;
+	            byte[] buffer = new byte[BUFFER_SIZE];
+	            while ((bytesRead = inputStream.read(buffer)) != -1) {
+	                outputStream.write(buffer, 0, bytesRead);
+	            }
+	 
+	            outputStream.close();
+	            inputStream.close();
+	 
+		            System.out.println("File downloaded");
+		        } else {
+		            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+		        }
+		        httpConn.disconnect();
+		    
 		  
 		  
 		  SAXBuilder builder = new SAXBuilder();
