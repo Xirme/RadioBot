50f8ce85d3265a6eee3d11fadd84ba89e8b0fdf3
Cyberpew <root@cyberpew.me>
2013-08-10 15:43:09 -0700

    Added a command displaying current song

--- src/me/xir/radio/radiobot/commands/Basic_Information.java
+++ src/me/xir/radio/radiobot/commands/Basic_Information.java
@@ -1,5 +1,10 @@
 package me.xir.radio.radiobot.commands;
 
+import java.io.File;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
@@ -7,8 +12,18 @@ import org.pircbotx.hooks.events.MessageEvent;
 public class Basic_Information extends ListenerAdapter {
 
 	public void onMessage(MessageEvent event) throws Exception {
+		
+		SAXBuilder builder = new SAXBuilder();
+		File xmlFile = new File("stats.xml");
+		
+		Document document = (Document) builder.build(xmlFile);
+		Element rootNode = document.getRootElement();
+		Element node = rootNode;
+		
+		
+		
 		if (event.getMessage().startsWith("song")) {
-			event.getBot().sendMessage(event.getChannel(), "Current song: NULL"); 
+			event.getBot().sendMessage(event.getChannel(), "Current song: " + node.getChildText("SONGTITLE")); 
 		}
 		if (event.getMessage().startsWith("listen")) {
 			event.getBot().sendMessage(event.getChannel(), "Listen here: NULL"); 
--- src/me/xir/radio/radiobot/utils/Radio_Query.java
+++ src/me/xir/radio/radiobot/utils/Radio_Query.java
@@ -6,42 +6,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import me.xir.radio.radiobot.Config;
-import me.xir.radio.radiobot.RadioBot;
 
-import org.apache.commons.codec.binary.Base64;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 import org.pircbotx.hooks.ListenerAdapter;
 
 @SuppressWarnings("rawtypes")
 public class Radio_Query extends ListenerAdapter {
 
 	public static void grabStreamXML() throws IOException {
-		/* FUCK JSOUP FOR NOW AND TRY JDOM
-		 * 
-		 *  final String link = "http://" + Config.scserver + ":" + Config.scport + "/admin.cgi?mode=viewxml";
 
-		String username = Config.scauser;
-		String password = Config.scapass;
-		String login = username + ":" + password;
-		String base64login = new String(Base64.encodeBase64(login.getBytes()));
-
-		Document doc = Jsoup.connect(link).userAgent("Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36").header("Authorization", "Basic " + base64login).timeout(10*1000).get();
-
-
-		// start grabbing the XML fields here.
-		  for (Element currentsong : doc.select("SONGTITLE")) {
-			  RadioBot.bot.sendMessage("#radio","Current Song: " + currentsong); 
-		    }
-	}
-		 */  
 		int BUFFER_SIZE = 4096;
 		String fileURL = "http://dev.cyberpew.me/stats.xml";
 		URL url = new URL(fileURL);
@@ -96,33 +72,6 @@ public class Radio_Query extends ListenerAdapter {
 		httpConn.disconnect();
 	}
 
-	public static void parseStreamXML() throws IOException {
-
-		SAXBuilder builder = new SAXBuilder();
-
-		File xmlFile = new File("stats.xml");
-		try {
-
-			Document document = (Document) builder.build(xmlFile);
-			Element rootNode = document.getRootElement();
-			List list = rootNode.getChildren();
-
-			System.out.println(list.size());
-
-				System.out.println("Executed!");
-
-				Element node = rootNode;
-
-				RadioBot.bot.sendMessage("#radio","Current Song: " + node.getChildText("SONGTITLE"));
-
-		} catch (IOException io) {
-			System.out.println(io.getMessage());
-		} catch (JDOMException jdomex) {
-			System.out.println(jdomex.getMessage());
-		}
-	}
-
-
 	public static void repeatQuery() {
 		int delay = 15000; // delay for 15 sec.
 		int period = 10000; // repeat every 10 sec.
@@ -133,7 +82,6 @@ public class Radio_Query extends ListenerAdapter {
 
 				try {
 					grabStreamXML();
-					parseStreamXML();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
